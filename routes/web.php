c16baf3db246c46e5f76ef75d0864b453a7a80e9
fabioNog <fabio.carmo@computacao.ufla.br>
2020-06-29 21:57:02 -0300

    Adicionando rota para get login e post login

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/admin', 'AdminController@index')->name('homeadmin');
+
+Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
+Route::get('/admin/login', 'Auth\AdminLoginController@index')->name('admin.login');
+Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+
 Route::get('/produtos', 'ProdutoController@index');
 Route::get('/usuarios', function(){
     return view('usuario');
