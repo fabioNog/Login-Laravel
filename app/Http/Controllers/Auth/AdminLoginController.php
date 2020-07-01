dd61530c5a5d4e08db7b0932795aaab2fb74914f
fabioNog <fabio.carmo@computacao.ufla.br>
2020-06-30 23:16:23 -0300

    Alterando o route

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminLoginController extends Controller
 {
@@ -13,7 +14,27 @@ class AdminLoginController extends Controller
     }
 
     public function login(Request $request){
-        return "ok";
+        $this->validate($request,[
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+
+
+        $authOk = Auth::guard('admin')->attempt($credentials,$request->remember);
+
+
+
+        if($authOk){
+            return redirect()->intended(route('admin.dashboard'));
+        }
+
+        return redirect()->back()->withInputs($request->only('email','remember'));
     }
 
     public function index(){
